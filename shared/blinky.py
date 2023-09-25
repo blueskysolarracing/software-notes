39875097e3c889d46ab93d0b6a8e5834947d4511
TristanRobitaille <robitaille.tristan@gmail.com>
2023-09-25 11:55:24 -0400

    Update blinky.py
    Added output for BFM's LED0

--- shared/blinky.py
+++ shared/blinky.py
@@ -4,12 +4,18 @@ from time import sleep
 from periphery import GPIO
 
 path = environ['path']
+path_BFM_LED0 = environ['path_BFM_LED0']
+
 line = int(environ['line'])
+line_BFM_LED0 = int(environ['line_BFM_LED0'])
+
 gpio = GPIO(path, line, 'out')
+gpio_BFM_LED0 = GPIO(path_BFM_LED0, line_BFM_LED0, 'out')
 value = True
 
 while True:
   gpio.write(value)
+  gpio_BFM_LED0.write(value)
   sleep(1)
 
   value = not value
