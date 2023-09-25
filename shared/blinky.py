5441b7682923a073e47d90c78aaaa5437b829eb5
TristanRobitaille <robitaille.tristan@gmail.com>
2023-09-25 12:02:59 -0400

    Update blinky.py
    Renamed path/line to have Ixora LED4 green. This is better because that GPIO is unused on BFM, so it cannot create unexpected/dangerous behaviour if the blinky runs while plugged onto BFM but still allows to see that blinky is running on Ixora.

--- shared/blinky.py
+++ shared/blinky.py
@@ -3,18 +3,18 @@ from time import sleep
 
 from periphery import GPIO
 
-path = environ['path']
+path_Ixora_LED4_GREEN = environ['path_Ixora_LED4_GREEN']
 path_BFM_LED0 = environ['path_BFM_LED0']
 
-line = int(environ['line'])
+line_Ixora_LED4_GREEN = int(environ['line_Ixora_LED4_GREEN'])
 line_BFM_LED0 = int(environ['line_BFM_LED0'])
 
-gpio = GPIO(path, line, 'out')
+gpio_Ixora_LED4_GREEN = GPIO(path_Ixora_LED4_GREEN, line_Ixora_LED4_GREEN, 'out')
 gpio_BFM_LED0 = GPIO(path_BFM_LED0, line_BFM_LED0, 'out')
 value = True
 
 while True:
-  gpio.write(value)
+  gpio_Ixora_LED4_GREEN.write(value)
   gpio_BFM_LED0.write(value)
   sleep(1)
 
