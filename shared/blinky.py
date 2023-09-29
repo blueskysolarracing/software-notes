0d45fa3ac21cc30eb9bc67fd523fd8bd039b4eed
Juho Kim <juho-kim@outlook.com>
2023-09-29 05:19:04 -0400

    Restore blinky.py, as portainer-blinky.md depends on it; move the BFM blinky as blinky_bfm.py.

--- shared/blinky.py
+++ shared/blinky.py
@@ -1,21 +1,12 @@
-from os import environ
-from time import sleep
-
 from periphery import GPIO
 
-path_Ixora_LED4_GREEN = environ['path_Ixora_LED4_GREEN']
-path_BFM_LED0 = environ['path_BFM_LED0']
-
-line_Ixora_LED4_GREEN = int(environ['line_Ixora_LED4_GREEN'])
-line_BFM_LED0 = int(environ['line_BFM_LED0'])
-
-gpio_Ixora_LED4_GREEN = GPIO(path_Ixora_LED4_GREEN, line_Ixora_LED4_GREEN, 'out')
-gpio_BFM_LED0 = GPIO(path_BFM_LED0, line_BFM_LED0, 'out')
+path = environ['path']
+line = int(environ['line'])
+gpio = GPIO(path, line, 'out')
 value = True
 
 while True:
-  gpio_Ixora_LED4_GREEN.write(value)
-  gpio_BFM_LED0.write(value)
+  gpio.write(value)
   sleep(1)
 
   value = not value
